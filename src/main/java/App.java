a45ffba0eadbf55ecb04e10fd9e06be093de5745
Igor Ejov <iezho@yandex.ru>
2025-01-21 19:54:50 +0300

    Initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,25 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat.getName());
+
+        Cat beanCat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanCat1.getName());
+
+        System.out.println("сравнение привет мир: "
+                + bean.equals(bean1));
+
+        System.out.println("сравнение кисок: "
+                + beanCat.equals(beanCat1));
     }
 }
